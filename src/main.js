2a17e90fd6aedfd19fbf8446f602fcab9e4da532
devmael <mael.maillard@gmail.com>
2019-05-29 17:01:31 +0200

    test carte

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Icon }  from 'leaflet'
+import * as Vue2Leaflet from 'vue2-leaflet'
+
+// this part resolve an issue where the markers would not appear
+delete Icon.Default.prototype._getIconUrl;
+
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+
+delete Icon.Default.prototype._getIconUrl;
+
+Vue.component('l-map', Vue2Leaflet.LMap)
+Vue.component('l-tile-layer', Vue2Leaflet.LTileLayer)
+
 
 Vue.config.productionTip = false
 
