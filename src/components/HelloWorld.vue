99f98a5db770b8c7a5ff0d3d777a914ef580ed09
devmael <mael.maillard@gmail.com>
2019-06-04 17:34:12 +0200

    calcul distance lat long

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,7 @@
     <div>
       <input v-model="address" placeholder="entrer une adresse">
       <button v-on:click="search">valider</button>
-      {{locate}}
+<!--      {{ locate}}-->
       {{hotels}}
     </div>
     <div class="info" style="height: 15%">
@@ -44,6 +44,28 @@
             };
         },
         methods: {
+          // https://www.geodatasource.com/developers/javascript
+          distance(lat1, lon1, lat2, lon2, unit) {
+          if ((lat1 == lat2) && (lon1 == lon2)) {
+          return 0;
+         }
+        else {
+          var radlat1 = Math.PI * lat1/180;
+          var radlat2 = Math.PI * lat2/180;
+          var theta = lon1-lon2;
+          var radtheta = Math.PI * theta/180;
+          var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+          if (dist > 1) {
+            dist = 1;
+          }
+          dist = Math.acos(dist);
+          dist = dist * 180/Math.PI;
+          dist = dist * 60 * 1.1515;
+          if (unit=="K") { dist = dist * 1.609344 }
+          if (unit=="N") { dist = dist * 0.8684 }
+          return dist;
+          }
+          },
             addMarker(event) {
                 this.markers.push(event.latlng);
             },
@@ -62,24 +84,33 @@
                         this.locate = response.data.features[0].geometry
                         this.centerUpdated([this.locate.coordinates[1],this.locate.coordinates[0]])
                         // this.marker = [this.locate.coordinates[1],this.locate.coordinates[0]]
-                        console.log('test')
+
                         this.searchHotel(this.locate.coordinates[1],this.locate.coordinates[0])
                         // this.markers.push([this.locate.coordinates[1],this.locate.coordinates[0]])
                     })
 
             },
-            addHotel(hotel){
-                this.hotels.push(hotel)
-            },
+
 
             searchHotel: function(lat,long){
 
                 //5510z est le naf pour les hotels
                 axios.get('https://entreprise.data.gouv.fr/api/sirene/v1/near_point/?lat='+lat+'&long='+long+'&activite_principale=5510Z')
                     .then(response => {
-                        this.hotels =response.data.etablissements
-                        this.hotels.forEach(function(hotel){
-                            this.markers.push([hotel.latitude,hotel.longitude])
+                        // this.hotels =response.data.etablissements
+
+                      response.data.etablissements.forEach(function(OneHotel){
+                        this.markers.push([OneHotel.latitude,OneHotel.longitude])
+                        var distance = this.distance(this.locate.coordinates[1],this.locate.coordinates[0],OneHotel.latitude,OneHotel.longitude,"K")
+                        var Hotel= {name: OneHotel.l1_normalisee,
+                                      adresse:OneHotel.geo_adresse,
+                                      latitude: OneHotel.latitude,
+                                      longitude: OneHotel.longitude ,
+                                      distance:distance ,
+                                      rechercheLat:this.locate.coordinates[1],
+                                      rechercheLong:this.locate.coordinates[0] }
+
+                        this.hotels.push(Hotel)
                         }.bind(this))
 
                     })
