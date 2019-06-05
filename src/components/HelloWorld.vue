dc75a5306561e46aa0c42586eb7219c2a5c703c6
devmael <mael.maillard@gmail.com>
2019-06-05 17:22:06 +0200

    debut css card

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,39 @@
 <template>
 
-  <div style="height: 100%; width: 100%">
-    <div>
-      <input v-model="address" placeholder="entrer une adresse">
-      <button v-on:click="search">valider</button>
-<!--      {{ locate}}-->
-      {{hotels}}
-    </div>
-    <div class="info" style="height: 15%">
-      <span>Center: {{ center }}</span>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <input v-model="address" placeholder="entrer une adresse">
+                <button v-on:click="search">valider</button>
+                <!--      {{ locate}}-->
+                <l-map
+                        style="height: 80%; width: 100%"
+                        :zoom="zoom"
+                        :center="center"
+                        @update:zoom="zoomUpdated"
+                        @update:center="centerUpdated"
+                        @update:bounds="boundsUpdated">
+                    <l-tile-layer :url="url"></l-tile-layer>
+                    <l-marker :lat-lng="marker" v-for=" marker in markers"></l-marker>
+                </l-map>
+            </div>
 
+        <div class="col-sm">
+            <div class="card" v-for="hotel in hotels">
+                <h5 class="card-title">{{hotel.name}}</h5>
+                <p class="card-text">{{hotel.adresse}}.</p>
+                <p class="card-text">{{hotel.distance}} km</p>
+            </div>
+        </div>
+        </div>
     </div>
-    <l-map
-            style="height: 80%; width: 100%"
-            :zoom="zoom"
-            :center="center"
-            @update:zoom="zoomUpdated"
-            @update:center="centerUpdated"
-            @update:bounds="boundsUpdated"
-    >
-      <l-tile-layer :url="url"></l-tile-layer>
-      <l-marker  :lat-lng="marker" v-for=" marker in markers"></l-marker>
-    </l-map>
-  </div>
+
 
 </template>
 
 <script>
     import axios from 'axios';
+
     export default {
         data() {
             return {
@@ -36,7 +42,7 @@
                 center: [0, 0],
                 bounds: null,
                 locate: '',
-                address:'',
+                address: '',
                 markers: [],
 
 
@@ -44,28 +50,31 @@
             };
         },
         methods: {
-          // https://www.geodatasource.com/developers/javascript
-          distance(lat1, lon1, lat2, lon2, unit) {
-          if ((lat1 == lat2) && (lon1 == lon2)) {
-          return 0;
-         }
-        else {
-          var radlat1 = Math.PI * lat1/180;
-          var radlat2 = Math.PI * lat2/180;
-          var theta = lon1-lon2;
-          var radtheta = Math.PI * theta/180;
-          var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-          if (dist > 1) {
-            dist = 1;
-          }
-          dist = Math.acos(dist);
-          dist = dist * 180/Math.PI;
-          dist = dist * 60 * 1.1515;
-          if (unit=="K") { dist = dist * 1.609344 }
-          if (unit=="N") { dist = dist * 0.8684 }
-          return dist;
-          }
-          },
+            // https://www.geodatasource.com/developers/javascript
+            distance(lat1, lon1, lat2, lon2, unit) {
+                if ((lat1 == lat2) && (lon1 == lon2)) {
+                    return 0;
+                } else {
+                    var radlat1 = Math.PI * lat1 / 180;
+                    var radlat2 = Math.PI * lat2 / 180;
+                    var theta = lon1 - lon2;
+                    var radtheta = Math.PI * theta / 180;
+                    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+                    if (dist > 1) {
+                        dist = 1;
+                    }
+                    dist = Math.acos(dist);
+                    dist = dist * 180 / Math.PI;
+                    dist = dist * 60 * 1.1515;
+                    if (unit == "K") {
+                        dist = dist * 1.609344
+                    }
+                    if (unit == "N") {
+                        dist = dist * 0.8684
+                    }
+                    return dist;
+                }
+            },
             addMarker(event) {
                 this.markers.push(event.latlng);
             },
@@ -82,55 +91,58 @@
                 axios.get('https://api-adresse.data.gouv.fr/search/?q=' + this.address)
                     .then(response => {
                         this.locate = response.data.features[0].geometry
-                        this.centerUpdated([this.locate.coordinates[1],this.locate.coordinates[0]])
+                        this.centerUpdated([this.locate.coordinates[1], this.locate.coordinates[0]])
                         // this.marker = [this.locate.coordinates[1],this.locate.coordinates[0]]
 
-                        this.searchHotel(this.locate.coordinates[1],this.locate.coordinates[0])
+                        this.searchHotel(this.locate.coordinates[1], this.locate.coordinates[0])
                         // this.markers.push([this.locate.coordinates[1],this.locate.coordinates[0]])
                     })
 
             },
 
 
-            searchHotel: function(lat,long){
+            searchHotel: function (lat, long) {
 
                 //5510z est le naf pour les hotels
-                axios.get('https://entreprise.data.gouv.fr/api/sirene/v1/near_point/?lat='+lat+'&long='+long+'&activite_principale=5510Z')
+                axios.get('https://entreprise.data.gouv.fr/api/sirene/v1/near_point/?lat=' + lat + '&long=' + long + '&activite_principale=5510Z')
                     .then(response => {
                         // this.hotels =response.data.etablissements
 
-                      response.data.etablissements.forEach(function(OneHotel){
-                        this.markers.push([OneHotel.latitude,OneHotel.longitude])
-                        var distance = this.distance(this.locate.coordinates[1],this.locate.coordinates[0],OneHotel.latitude,OneHotel.longitude,"K")
-                        var Hotel= {name: OneHotel.l1_normalisee,
-                                      adresse:OneHotel.geo_adresse,
-                                      latitude: OneHotel.latitude,
-                                      longitude: OneHotel.longitude ,
-                                      distance:distance ,
-                                      rechercheLat:this.locate.coordinates[1],
-                                      rechercheLong:this.locate.coordinates[0] }
+                        response.data.etablissements.forEach(function (OneHotel) {
+                            this.markers.push([OneHotel.latitude, OneHotel.longitude])
+                            var distance = this.distance(this.locate.coordinates[1], this.locate.coordinates[0], OneHotel.latitude, OneHotel.longitude, "K")
+                            var Hotel = {
+                                name: OneHotel.l1_normalisee,
+                                adresse: OneHotel.geo_adresse,
+                                latitude: OneHotel.latitude,
+                                longitude: OneHotel.longitude,
+                                distance: distance,
+                                rechercheLat: this.locate.coordinates[1],
+                                rechercheLong: this.locate.coordinates[0]
+                            }
 
-                        this.hotels.push(Hotel)
+                            this.hotels.push(Hotel)
                         }.bind(this))
-
+                        this.hotels=this.hotels.sort()
                     })
-                    //
-
+                //
+                this.hotels=this.hotels.sort(function(a, b) {
+                    return a - b
+                })
 
             }
         },
 
 
-
     }
 </script>
+
 <style>
-  #app,
-  l-map {
-    position: relative;
-    padding: 0;
-    width: 600px;
-    height: 600px;
-  }
+    #app,
+    l-map {
+        position: relative;
+        padding: 0;
+        height: 50vh;
+    }
 
 </style>
